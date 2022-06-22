34ade1cc195211539501abcd44b4f42d9922c9e9
Mike Rae <mikerae@me.com>
2022-06-22 15:43:19 +0000

    Add initial js
    Add eventlistner to call main() when page loads.
    
    main() does the following:
    Intialise all CONSTANTS, top level variables, classes and objects.
    Ensure all varriables have local scope.
    Ensure all varriables have local scope.
    Call the Wecome Screen.
    Reset the game when called.

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Wait for the DOM to finish loading before running the Main  wrapper function
+document.addEventListener("DOMContentLoaded", main());
+
+/**
+ * Intialise all CONSTANTS, top level variables, classes and objects.
+ * Ensure all varriables have local scope.
+ * Ensure all varriables have local scope.
+ * Call the Wecome Screen.
+ * Reset the game when called.
+ */
+function main() {
+  // Initialsie Classes of Objects
+
+  // Initialise CONSTANTS
+
+  // Initialise local variables
+
+  //Initialse Objects
+
+  //Display Welcome Screen
+
+}

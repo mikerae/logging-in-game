d0de360a2e3724802e6ebf994c768a538a46a4ea
Mike Rae <mikerae@me.com>
2022-06-22 18:43:22 +0000

    Add function to toggle display screens
    Perameters: screen, displayScreen-wrapper element, and
    displayGame-wrapper element.
    When screen is 'game'  displayScreen-wrapper is hidden,
    and the displayGame-wrapper is shown.
    And vica versa

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,38 @@ function main() {
   // Initialsie Classes of Objects
 
   // Initialise CONSTANTS
+  let SCREENS = ["welcome", "intro", "game", "win"];
+  let MONOLOGUEDISPLAY = document.getElementById("displayScreen-wrapper");
+  let GAMEDISPLAY = document.getElementById("displayGame-wrapper");
 
   // Initialise local variables
+  let screen = SCREENS[0];
 
   //Initialse Objects
 
   //Display Welcome Screen
+  setDisplay(screen, GAMEDISPLAY, MONOLOGUEDISPLAY);
+}
 
+// Screen Utility Functions
+
+/**
+ * Toggels the display between Monologue screens 
+ * (welcome, intro, win) and Game screen
+ */
+function setDisplay(screen, GAMEDISPLAY, MONOLOGUEDISPLAY) {
+  switch (screen) {
+    case 'game':
+      MONOLOGUEDISPLAY.style.display = "none";
+      GAMEDISPLAY.style.removeProperty('display');
+      break;
+    case 'welcome':
+    case 'intro':
+    case 'win':
+      MONOLOGUEDISPLAY.style.removeProperty('display');
+      GAMEDISPLAY.style.display = "none";
+      break;
+    default:
+      alert("Im sorry, there is a problem with the game! Contact Mike for assistance");
+  }
 }
